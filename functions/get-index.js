e943e81855dba7f5e5224a2bb31066dc4ff7968e
Andrea Bisognin <andrea.bisognin@scaiitec.it>
2020-04-10 09:02:02 +0200

    populate homepage

--- functions/get-index.js
+++ functions/get-index.js
@@ -4,6 +4,11 @@
 const co = require("co");
 const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
+const Mustache = require('mustache');
+const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const http = require('superagent-promise')(require ('superagent'), Promise);
+
+const restaurantsApiroot = process.env.restaurants_api;
 
 var html;
 
@@ -14,9 +19,16 @@ function* loadHtml() {
   return html;
 }
 
+function* getRestaurants() {
+  return (yield http.get(restaurantsApiroot)).body;
+}
+
 module.exports.handler = co.wrap(function* (event, context, callback)  {
   
-  let html = yield loadHtml();
+  let template = yield loadHtml();
+  let restaurants = yield getRestaurants();
+  let dayOfWeek = Days[new Date().getDay()];
+  let html = Mustache.render(template, { dayOfWeek, restaurants });
 
   const response =  {
     statusCode: 200,
