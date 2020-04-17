f4e1c4463e9894a1309322d70e4f89d71465ed90
Andrea Bisognin <andrea.bisognin@gmail.com>
2020-04-17 18:18:03 +0200

    added build script

--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -23,7 +23,13 @@ function* findRestaurantsByTheme(theme, count) {
 }
 
 module.exports.handler = co.wrap(function* (event, context, cb){
-    let req = JSON.parse(new Buffer(event.body,"base64").toString("ascii"));
+    let body = event.body;
+    
+    if (event.isBase64Encoded){
+        body = new Buffer(event.body,"base64").toString("ascii");
+    }
+
+    let req = JSON.parse(body);
 
     let restaurants = yield findRestaurantsByTheme(req.theme, defaultResults);
     let response = {
