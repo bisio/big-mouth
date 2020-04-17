8d8d6c90d9243d1c8310977a0f32fdfa3149bb21
Andrea Bisognin <andrea.bisognin@scaiitec.it>
2020-04-17 21:40:19 +0200

    added buildspec and fixed acceptance test

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,6 +1,7 @@
 'use strict';
 const co = require('co');
 const Promise = require('bluebird');
+const awscred = Promise.promisifyAll(require('awscred'));
 
 
 let initialized = false;
@@ -16,6 +17,11 @@ let init = co.wrap(function* (){
     process.env.cognito_user_pool_id = "eu-west-1_XUJlg27NJ";
     process.env.cognito_server_client_id = "2qegmh9bq94abltrvbhe019r1i";
     
+    if (!process.env.AWS_ACCESS_KEY_ID) {
+        let cred = (yield awscred.loadAsync()).credentials;
+        process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
+        process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
+      }
 
     console.log("AWS credentials loaded");
 
